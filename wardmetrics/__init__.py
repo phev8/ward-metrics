2c7dc8a361daf5fb7041b7dced15cd4eb39df42e
Peter Hevesi <peter.hevesi@dfki.de>
2017-04-25 13:50:54 +0200

    Read version information

--- wardmetrics/__init__.py
+++ wardmetrics/__init__.py
@@ -1,6 +1,1 @@
-from pkg_resources import get_distribution, DistributionNotFound
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-   # package is not installed
-   pass
+from .version import __version__
