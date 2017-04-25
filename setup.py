9c9427751b95e68e0b7cc058522434c82cfb060d
Peter Hevesi <peter.hevesi@dfki.de>
2017-04-25 13:11:44 +0200

    Fix setup.py descriptions

--- setup.py
+++ setup.py
@@ -41,18 +41,15 @@ setup(
         'Development Status :: 3 - Alpha',
 
         # Indicate who your project is intended for
-        'Intended Audience :: Scientists',
+        'Intended Audience :: Science/Research',
         'Topic :: Machine Learning :: Evaluation Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: MIT License',
+        'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only'
     ],
 
     # What does your project relate to?
